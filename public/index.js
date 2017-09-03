c264a9a1ffc8ac497770a17c17adde6fbad8b67b
viktor Xu <xulai1001@gmail.com>
2017-09-04 00:36:40 +0800

    add fade in for big img, change bootstrap site

--- public/index.js
+++ public/index.js
@@ -4,18 +4,42 @@ $(document).ready(function() {
         el: "#character",
         data: {
             chr: {
-                BigImage: "",
+                BigImage: "",   // unused
                 SmallImage: "",
                 GroupName: "",
                 RoleName: ""
-            }
+            },
+            bigimg: ""
         }
     });
+
+    var img_preload = new Image();
+    img_preload.onload = function() {
+        $("#bigimg").fadeTo(700, 0.3, function() {
+            character.bigimg = img_preload.src;
+        }).fadeTo(1000, 1);        
+    };
+
+    var query_img = function(name) {
+        $.ajax({
+            type: "GET",
+            url: "query_chr?q=" + name,
+            async: true,
+            success: function(res) {
+                if (res && res["success"]) {
+                    character.chr = res;
+                    img_preload.src = res["BigImage"];
+                    $("#start_wrapper").css("display", "none");
+                    $(".main_wrapper").css("display", "block");
+                }
+            }
+        });
+    };
 
     $("#chr_name").autocomplete({
         source: function(request, response) {
             var name = request.term.trim();
-            if (name && name.length >= 1)
+            if (name.length>=1) {
                 $.ajax({
                     type: "GET",
                     url: "query_name?q=" + name,
@@ -26,43 +50,23 @@ $(document).ready(function() {
                         } else response([]);
                     }
                 });
-            else
-                response([]);
-        }
+            } else { response([]); }
+        },
+        minChars: 1,
+        scrollHeight: 300,
+        select: function(event, ui) { query_img(ui.item.value); } 
     });
 
     $("#chr_name").keydown(function(e) {
         event = event || window.event;
-        if (event.keyCode == 13) {
-            var name = $("#chr_name").val();
-            $.ajax({
-                type: "GET",
-                url: "query_chr?q=" + name,
-                async: true,
-                success: function(res) {
-                    if (res && res["success"]) {
-                        character.chr = res;
-                        $("#start_wrapper").css("display", "none");
-                        $(".main_wrapper").css("display", "block");
-                    }
-                }
-            });
-        }
+        if (event.keyCode == 13) { query_img($("#chr_name").val()); }
     });
 
-    $("#btn_submit").click(function() {
-        var name = $("#chr_name").val();
-        $.ajax({
-            type: "GET",
-            url: "query_chr?q=" + name,
-            async: true,
-            success: function(res) {
-                if (res && res["success"]) {
-                    character.chr = res;
-                    $("#start_wrapper").css("display", "none");
-                    $(".main_wrapper").css("display", "block");
-                }
-            }
-        });
+    $("#btn_submit").click(function () { query_img($("#chr_name").val()); });
+
+    $("#bigimg").load(function() {
+        $(this).hide;
+        $(this).fadeIn(800); 
     });
+
 });

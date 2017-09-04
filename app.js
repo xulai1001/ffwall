be593aa00a91c350ef28fd5eaaefba0cafa85071
songch <songch331@126.com>
2017-09-04 21:13:53 +0800

    change to partial view

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.set("view engine", "html");
 
 app.get('/query_name', function(req, res) {
     console.time("query_name");
-    var q="%" + req.query.q + "%";
+    var q = "%" + req.query.q + "%";
     var query = db.prepare("select * from ffwall where RoleName like $q");
-    query.all({$q: q}, function (err, result) {
+    query.all({ $q: q }, function(err, result) {
         if (!err) {
             console.log("query_name " + req.query.q + " - " + result.length + " results");
             console.timeEnd("query_name");
@@ -28,29 +28,44 @@ app.get('/query_chr', function(req, res) {
     console.time("query_chr");
     console.log("query_chr " + req.query.q);
 
-    var cond = "Id = $q", q = req.query.q;
+    var cond = "Id = $q",
+        q = req.query.q;
     if (isNaN(req.query.q)) {
         cond = "RoleName = $q";
     } else {
-        q=parseInt(q);
-    }  
+        q = parseInt(q);
+    }
 
     var query = db.prepare("select * from ffwall where " + cond);
-    query.get({$q: q}, function(err, result) {
+    query.get({ $q: q }, function(err, result) {
         if (!err && result) {
-                result["success"] = true;
-                console.log(result);
-                res.json(result);
-                console.timeEnd("query_chr");
+            result["success"] = true;
+            console.log(result);
+            res.json(result);
+            console.timeEnd("query_chr");
         } else {
             if (err) console.log(err);
-            res.json({success: false});
+            res.json({ success: false });
         }
     });
 });
 
+app.get('/query_server', function(req, res) {
+    console.time("query_server");
+    var q = "%" + req.query.q + "%";
+    var query = db.prepare("select * from ffwall where GroupName like $q");
+    query.all({ $q: q }, function(err, result) {
+        if (!err) {
+            console.log("query_server " + req.query.q + " - " + result.length + " results");
+            console.timeEnd("query_server");
+            res.json(result);
+        } else { console.log(err); }
+    });
+    query.finalize();
+});
+
 app.get('/:id?', function(req, res) {
-    res.render("index", {id: req.params.id});
+    res.render("index", { id: req.params.id });
 });
 //app.use(express.static("static"));
 
